de222ead6ffa6b58463e9ca62c6ef59fedd39f96
Mika <amethyst602@gmail.com>
2024-05-26 19:24:03 +0800

    fix:修改CSS路徑吃不到的問題

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 设置基础路径
   plugins: [
     vue(),
     VueDevTools(),
@@ -17,5 +17,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src')
     }
   },
-  
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 })
