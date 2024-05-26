ca0597422510d85daf9e8466468f23ed0d44da50
Mika <amethyst602@gmail.com>
2024-05-26 23:11:13 +0800

    feat:menu改抓router跑資料

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,30 +1,21 @@
 <template>
 	<div class="menu">
-		<!-- <RouterLink to="/profiles">作品集</RouterLink> -->
 		<el-menu class="el-menu-demo" mode="horizontal">
-			<el-sub-menu index="1">
-				<template #title>小工具</template>
-				<el-menu-item index="1-1">
-					<RouterLink to="/lottery">抽獎</RouterLink>
-				</el-menu-item>
-				<el-menu-item>
-					<RouterLink to="/calendar">日曆</RouterLink>
+			<el-sub-menu :index="key" v-for="(parent, key) in menu.data">
+				<template #title>{{ parent.name }}</template>
+				<el-menu-item index="1-1" v-for="(child, key2) in parent.children">
+					<RouterLink :to="child.path">{{ child.name }}</RouterLink>
 				</el-menu-item>
 			</el-sub-menu>
-			<!-- <el-sub-menu index="1">
-				<template #title>讀書會</template>
-				<el-menu-item index="1-1">
-					<RouterLink to="/lottery">抽獎</RouterLink>
-				</el-menu-item>
-				<el-menu-item>
-					<RouterLink to="/calendar">日曆</RouterLink>
-				</el-menu-item>
-			</el-sub-menu> -->
 		</el-menu>
 	</div>
 </template>
 <script setup>
+	import { reactive } from "vue";
 	import { RouterLink, RouterView } from "vue-router";
+	import router from "../router";
+	const menu = reactive({ data: [...router.options.routes] });
+	console.log("hi", menu);
 </script>
 <style lang="scss" scoped>
 	a {
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,15 +4,22 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
-      path: '/lottery',
-      name: 'lottery',
-      component: () => import('../views/lottery/index.vue')
+      path:'/tools',
+      name:'小工具',
+      children:[
+        {
+          path: '/lottery',
+          name: 'lottery',
+          component: () => import('../views/lottery/index.vue')
+        },
+        {
+          path: '/calendar',
+          name: '日曆',
+          component: () => import('../views/calendar/index.vue')
+        }
+      ]
     },
-    {
-      path: '/calendar',
-      name: '日曆',
-      component: () => import('../views/calendar/index.vue')
-    }
+   
   ]
 })
 
